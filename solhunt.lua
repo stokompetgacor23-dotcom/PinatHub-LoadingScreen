1ff297ef767e7e24bbced65a087efd82b4a549aa
stokompetgacor23-dotcom <stokompetgacor23@gmail.com>
2026-06-11 12:31:26 +0700

    Update solhunt.lua

--- solhunt.lua
+++ solhunt.lua
@@ -211,7 +211,7 @@ end)
 
 UIS.InputChanged:Connect(function(input)
     if dragging and dragStart and startPos then
-        if input.UserInputType == Enum.UserInputType.MouseMovement then
+        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
             local delta = input.Position - dragStart
             local newX = startPos.X.Offset + delta.X
             local newY = startPos.Y.Offset + delta.Y
